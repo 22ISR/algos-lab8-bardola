d4529ac50e5cf0783f14293e120713955be40b04
XaoriGit <xaoriNit@gmail.com>
2024-12-10 12:21:50 +0600

    in progress

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ monthly_spending = [2689.56, 2770.38, 2394.04, 2099.91, 3182.20, 3267.12, 1746.8
 ```  
 Напишите программу, которая с помощью цикла `for` вычисляет средние расходы Джона за первый семестр (январь–июнь) и второй семестр (июль–декабрь).  
 
+monthly_spending = [2689.56, 2770.38, 2394.04, 2099.91, 3182.20, 3267.12, 1746.83, 2545.72, 3328.20, 3147.30, 2462.61, 3890.45]
+
+first = monthly_spending[0:6]
+second = monthly_spending[5:]
+first_res = 1
+second_res = 1
+
+for i in first:
+    first_res = (first_res + i) / 2
+
+for i in second:
+    second_res = (second_res + i) / 2
+
+
+print(round(first_res, 2), round(second_res, 2))
+"""
+
+"""
+
 ---
 
 ### Задача 2: Кто тратил больше?  
@@ -15,6 +34,22 @@ john_monthly_spending = [2689.56, 2770.38, 2394.04, 2099.91, 3182.20, 3267.12, 1
 sam_monthly_spending = [1969.62, 3939.37, 2241.59, 3968.27, 3068.80, 1755.02, 3885.66, 2491.67, 3828.49, 3171.32, 2771.32, 3380.37]
 ```  
 Напишите программу, которая сравнивает расходы Джона и Сэма по месяцам и подсчитывает количество месяцев, в которых Джон тратил больше.  
+
+john_monthly_spending = [2689.56, 2770.38, 2394.04, 2099.91, 3182.20, 3267.12, 1746.83, 2545.72, 3328.20, 3147.30, 2462.61, 3890.45]  
+sam_monthly_spending = [1969.62, 3939.37, 2241.59, 3968.27, 3068.80, 1755.02, 3885.66, 2491.67, 3828.49, 3171.32, 2771.32, 3380.37]
+
+john = 0
+
+for index, i in enumerate(john_monthly_spending):
+    if i > sam_monthly_spending[index]:
+        john += 1
+
+
+print(f"Джон тратит больше в {john} месяцах")
+"""
+
+
+"""
 
 ---
 
@@ -26,13 +61,41 @@ tina_friends = ["Tim", "Susan", "Mary", "Josh"]
 ```  
 Объедините оба списка в один, исключив дублирующиеся имена.  
 
+
+paul_friends = ["Mary", "Tim", "Mike", "Henry"]  
+tina_friends = ["Tim", "Susan", "Mary", "Josh"]
+
+res = list(set(paul_friends + tina_friends))
+
+print(res)
+"""
+
+
+
+"""
 ---
 
 ### Задача 4: Общие друзья  
 Используя те же списки друзей Пола и Тины, напишите программу, которая с помощью цикла находит их общих друзей.  
 
+
 ---
 
+paul_friends = ["Mary", "Tim", "Mike", "Henry"]  
+tina_friends = ["Tim", "Susan", "Mary", "Josh"]
+
+common_friends = []
+
+for i in paul_friends:
+    if i in tina_friends:
+        common_friends.append(i)
+
+
+print(common_friends)
+"""
+
+
+"""
 ### Задача 5: Игроки в баскетбол  
 В спортивном клубе зарегистрированы игроки:  
 ```python
@@ -42,6 +105,21 @@ basketball_players = {"Eve", "Richard", "Jessica", "Sam", "Michael"}
 ```  
 Напишите программу, которая определяет игроков, зарегистрированных только в баскетболе (не в футболе и не в волейболе).  
 
+football_players = {"Eve", "Tom", "Richard", "Peter"}  
+volleyball_players = {"Jack", "Hugh", "Peter", "Sam"}  
+basketball_players = {"Eve", "Richard", "Jessica", "Sam", "Michael"}
+
+only_football = []
+
+for i in football_players:
+    if i not in volleyball_players and i not in basketball_players:
+        only_football.append(i)
+
+print(only_football)
+"""
+
+
+"""
 ---
 
 ### Задача 6: Подсчёт голосов  
@@ -51,6 +129,22 @@ poll_results = ["Python", "Java", "Javascript", "Python", "Javascript", "Python"
 ```  
 Используя словарь, подсчитайте количество голосов за каждый язык.  
 
+poll_results = ["Python", "Java", "Javascript", "Python", "Javascript", "Python", "C", "Python", "Python", "C", "Javascript"]
+
+set_poll = list(set(poll_results))
+
+dict_polls = {}
+
+for i in set_poll:
+    count = poll_results.count(i)
+    dict_polls[i] = count
+
+
+print(dict_polls)
+"""
+
+
+"""
 ---
 
 ### Задача 7: Подсчёт очков  
@@ -60,6 +154,19 @@ scores = [('Mike', 10), ('Mike', 8), ('Mike', 6), ('John', 7), ('John', 8), ('Jo
 ```  
 Создайте словарь, где ключами будут имена игроков, а значениями — их суммарные очки.  
 
+scores = [('Mike', 10), ('Mike', 8), ('Mike', 6), ('John', 7), ('John', 8), ('John', 5), ('Tom', 8), ('Tom', 9), ('Tom', 8)]
+
+dict_scores = {}
+
+for i in scores:
+    item = dict_scores.get(i[0], 0)
+    dict_scores.update({i[0]: item + i[1]})
+
+print(dict_scores)
+"""
+
+
+"""
 ---
 
 ### Задача 8: Статистика списка  
@@ -68,7 +175,19 @@ scores = [('Mike', 10), ('Mike', 8), ('Mike', 6), ('John', 7), ('John', 8), ('Jo
 numbers = [10, 3, 5, 9, 18, 3, 0, 7]
 ```  
 Напишите функцию, которая возвращает максимальное значение, сумму и среднее арифметическое чисел в списке.  
+"""
 
+numbers = [10, 3, 5, 9, 18, 3, 0, 7]
+
+def I_LOVE_PYTHON(numbers):
+    res = {}
+    res.update({"max": max(numbers), "sum": sum(numbers), "AVG": sum(numbers) / len(numbers)})
+    return res
+
+print(I_LOVE_PYTHON(numbers=numbers))
+
+
+"""
 ---
 
 ### Задача 9: Длинные и короткие слова  
@@ -77,7 +196,21 @@ numbers = [10, 3, 5, 9, 18, 3, 0, 7]
 word_list = ["apple", "airplane", "carrot", "elephant", "guitar", "moonlight"]
 ```  
 Напишите программу, которая определяет самое длинное и самое короткое слово в списке.  
+"""
 
+word_list = ["apple", "airplane", "carrot", "elephant", "guitar", "moonlight"]
+
+long_word = ""
+short_word = ""
+
+for i in word_list:
+    if len(i) > len(long_word):
+        long_word = i
+    if len(i) < len(short_word)
+
+print(long_word, short_word)
+
+"""
 ---
 
 ### Задача 10: Фильтрация по частоте  
